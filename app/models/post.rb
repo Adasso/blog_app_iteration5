66444d9601a25cb32b96bab1c28626f3f027b481
Alvaro Dasso <alvaro.dasso@outlook.com>
2016-10-06 16:43:03 -0600

    Iteration 5 without JS working

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  validates_presence_of :title
-  validates_presence_of :user_id
-  validates_presence_of :body
+  validates :title, presence: true
+  validates :user_id, presence: true
+  validates :body, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-#  validates_presence_of :first_name
-#  validates_presence_of :last_name
-#  validates_confirmation_of :email
-#  validates_length_of :password, :in =>8..20
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates_length_of :password, :in =>8..20
 end
